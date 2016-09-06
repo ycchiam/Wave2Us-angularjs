90b15cd91da8e812b09ab93a07321bfb1bc11c8a
Yee Chin, Chiam <phathetique@gmail.com>
2016-09-06 01:22:21 -0400

    Wired up login and logout function

--- app/index.js
+++ app/index.js
@@ -1,15 +1,61 @@
-(function() {
-	'use strict';
-	angular.module('wave2us', ['ngRoute', 'ngMaterial', 'wave2us.home', 'wave2us.login'])
+(function () {
+    'use strict';
 
-	.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-		  $locationProvider.hashPrefix('!');
-		  $routeProvider.otherwise({redirectTo: '/home'});
-		}])
+    // Firebase Config
+    var config = {
+        apiKey: "AIzaSyA7uEQKxfRbiwMuX7GkLWB01mwtP_IBiAw",
+        authDomain: "wave-2-us.firebaseapp.com",
+        databaseURL: "https://wave-2-us.firebaseio.com",
+        storageBucket: "wave-2-us.appspot.com"
+    };
 
-	.controller('IndexCtrl', IndexCtrl);
+    // Initialize Firebase
+    firebase.initializeApp(config);
+    var db = firebase.database();
+    var auth = firebase.auth();
 
-	function IndexCtrl($scope) {
-		$scope.currentNavItem = 'home';
-	}
+    // Shared
+    var activeUser = {
+        email: null,
+        displayName: null,
+        show: false
+    };
+
+    angular.module('wave2us', ['ngRoute', 'ngMaterial', 'wave2us.home', 'wave2us.login', 'wave2us.register'])
+
+    .config(function ($locationProvider, $routeProvider) {
+        $locationProvider.hashPrefix('!');
+        $routeProvider.otherwise({
+            redirectTo: '/home'
+        });
+    })
+
+    .controller('IndexCtrl', function ($scope) {
+        $scope.currentNavItem = window.location.href.split('#!/')[1];
+
+        $scope.activeUser = activeUser;
+        auth.onAuthStateChanged(function (user) {
+            if (user) {
+                // User is signed in.
+                $scope.activeUser.show = true;
+            } else {
+                // No user is signed in.
+                $scope.activeUser.show = false;
+            }
+            $scope.$apply();
+            console.log("User logged in: " + $scope.activeUser.show);
+        });
+
+        $scope.logout = function () {
+            firebase.auth().signOut();
+        }
+
+        var originatorEv;
+        this.openMenu = function ($mdOpenMenu, ev) {
+            originatorEv = ev;
+            $mdOpenMenu(ev);
+        };
+    })
+
+
 })();
--- app/register/register.js
+++ app/register/register.js
@@ -1,23 +1,23 @@
 (function () {
     'use strict';
-    angular.module('wave2us.login', ['ngRoute', 'ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
+    angular.module('wave2us.register', ['ngRoute', 'ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
 
     .config(function ($routeProvider, $mdThemingProvider) {
-        $routeProvider.when('/login', {
-            templateUrl: 'login/login.html',
-            controller: 'LoginCtrl'
+        $routeProvider.when('/register', {
+            templateUrl: 'register/register.html',
+            controller: 'RegisterCtrl'
         });
         $mdThemingProvider.theme('docs-dark', 'default')
             .primaryPalette('yellow')
             .dark();
     })
 
-    .controller('LoginCtrl', function ($scope) {
+    .controller('RegisterCtrl', function ($scope) {
         $scope.user = {};
         $scope.SignIn = function () {
             var email = $scope.user.email;
             var password = $scope.user.password;
-            firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+            firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
                 // Handle Errors here.
                 var errorCode = error.code;
                 var errorMessage = error.message;

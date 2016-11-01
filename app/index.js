eb6558583dadb6c05b78b0a2266a93ec2b288486
Yee Chin, Chiam <yee_chiam@freddiemac.com>
2016-11-01 17:37:53 -0400

    Changed home page design

--- app/index.js
+++ app/index.js
@@ -1,67 +1,68 @@
 /*global firebase, window*/
 
 (function () {
-    'use strict';
+	'use strict';
 
-    // Firebase Config
-    var config = {
-        apiKey: 'AIzaSyA7uEQKxfRbiwMuX7GkLWB01mwtP_IBiAw',
-        authDomain: 'wave-2-us.firebaseapp.com',
-        databaseURL: 'https://wave-2-us.firebaseio.com',
-        storageBucket: 'wave-2-us.appspot.com'
-    };
+	// Firebase Config
+	var config = {
+		apiKey: 'AIzaSyA7uEQKxfRbiwMuX7GkLWB01mwtP_IBiAw',
+		authDomain: 'wave-2-us.firebaseapp.com',
+		databaseURL: 'https://wave-2-us.firebaseio.com',
+		storageBucket: 'wave-2-us.appspot.com'
+	};
 
-    // Initialize Firebase
-    firebase.initializeApp(config);
-    var db = firebase.database();
-    var auth = firebase.auth();
+	// Initialize Firebase
+	firebase.initializeApp(config);
+	var db = firebase.database();
+	var auth = firebase.auth();
 
-    angular.module('wave2us', ['ngRoute', 'ngMaterial', 'firebase', 'wave2us.home', 'wave2us.login', 'wave2us.register', 'wave2us.profile'])
+	angular.module('wave2us', ['ngRoute', 'ngMaterial', 'firebase', 'wave2us.home', 'wave2us.login', 'wave2us.register', 'wave2us.profile'])
 
-    .factory('Auth', ['$firebaseAuth',
+	.factory('Auth', ['$firebaseAuth',
 	  function ($firebaseAuth) {
-            return $firebaseAuth();
+			return $firebaseAuth();
 	  }
 	])
 
-    .run(['$rootScope', '$location', function ($rootScope, $location) {
-        $rootScope.$on('$routeChangeError', function (event, next, previous, error) {
-            // We can catch the error thrown when the $requireSignIn promise is rejected
-            // and redirect the user back to the home page
-            if (error === 'AUTH_REQUIRED') {
-                $location.path('/home');
-            }
-        });
+	.run(['$rootScope', '$location', function ($rootScope, $location) {
+		$rootScope.$on('$routeChangeError', function (event, next, previous, error) {
+			// We can catch the error thrown when the $requireSignIn promise is rejected
+			// and redirect the user back to the home page
+			if (error === 'AUTH_REQUIRED') {
+				$location.path('/home');
+			}
+		});
     }])
 
-    .config(['$locationProvider', '$routeProvider', '$mdThemingProvider', function ($locationProvider, $routeProvider, $mdThemingProvider) {
-        $locationProvider.hashPrefix('!');
-        $routeProvider.otherwise({
-            redirectTo: '/home'
-        });
+	.config(['$locationProvider', '$routeProvider', '$mdThemingProvider', function ($locationProvider, $routeProvider, $mdThemingProvider) {
+		$locationProvider.hashPrefix('!');
+		$routeProvider.otherwise({
+			redirectTo: '/home'
+		});
 
-        $mdThemingProvider.theme('default')
-            .primaryPalette('pink')
-            .accentPalette('blue');
+		$mdThemingProvider.theme('default')
+			.primaryPalette('green')
+			.accentPalette('light-green')
+			.backgroundPalette('grey');
     }])
 
-    .controller('IndexCtrl', ['$scope', '$rootScope', 'Auth', function ($scope, $rootScope, Auth) {
-        $scope.currentNavItem = window.location.href.split('#!/')[1];
+	.controller('IndexCtrl', ['$scope', '$rootScope', 'Auth', function ($scope, $rootScope, Auth) {
+		$scope.currentNavItem = window.location.href.split('#!/')[1];
 
-        $scope.auth = Auth;
+		$scope.auth = Auth;
 
-        $scope.auth.$onAuthStateChanged(function (firebaseUser) {
-            $rootScope.firebaseUser = firebaseUser;
-        });
+		$scope.auth.$onAuthStateChanged(function (firebaseUser) {
+			$rootScope.firebaseUser = firebaseUser;
+		});
 
-        $scope.logout = function () {
-            firebase.auth().signOut();
-        };
+		$scope.logout = function () {
+			firebase.auth().signOut();
+		};
 
-        var originatorEv;
-        this.openMenu = function ($mdOpenMenu, ev) {
-            originatorEv = ev;
-            $mdOpenMenu(ev);
-        };
+		var originatorEv;
+		this.openMenu = function ($mdOpenMenu, ev) {
+			originatorEv = ev;
+			$mdOpenMenu(ev);
+		};
     }]);
 })();
